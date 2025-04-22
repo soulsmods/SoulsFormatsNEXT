70bfe20ed7de537985caa2682eedec1456240011
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2025-04-22 09:24:41 -0400

    Added LDMU format, updated SFUtil Zlib support options

--- SoulsFormats/Util/SFUtil.cs
+++ SoulsFormats/Util/SFUtil.cs
@@ -312,15 +312,58 @@ namespace SoulsFormats
         {
             br.AssertByte(0x78);
             br.AssertByte(0x01, 0x5E, 0x9C, 0xDA);
-            byte[] compressed = br.ReadBytes(compressedSize - 2);
+            return DecompressZlibBytes(br.ReadBytes(compressedSize - 2));
+        }
 
-            using (var decompressedStream = new MemoryStream())
+        /// <summary>
+        /// Decompresses zlib starting at the current position in a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="stream">A <see cref="Stream"/>.</param>
+        /// <param name="compressedSize">The size of the compressed data including the 2 byte zlib header.</param>
+        /// <returns>Decompressed zlib data.</returns>
+        /// <exception cref="EndOfStreamException">Cannot read beyond the end of the stream.</exception>
+        /// <exception cref="InvalidDataException">A valid zlib header could not be detected.</exception>
+        /// <exception cref="Exception">Did not read the expected number of compressed bytes from the <see cref="Stream"/>.</exception>
+        public static byte[] DecompressZlib(Stream stream, int compressedSize)
+        {
+            var cmf = stream.ReadByte();
+            var flg = stream.ReadByte();
+
+            if (cmf == -1 || flg == -1)
             {
-                using (var compressedStream = new MemoryStream(compressed))
-                using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress, true))
-                {
-                    deflateStream.CopyTo(decompressedStream);
-                }
+                throw new EndOfStreamException("Cannot read beyond the end of the stream.");
+            }
+
+            if (cmf != 0x78)
+            {
+                throw new InvalidDataException("Zlib header could not be detected.");
+            }
+
+            if (flg != 0x01 && flg != 0x5E && flg != 0x9C && flg != 0xDA)
+            {
+                throw new InvalidDataException("Valid zlib compression level could not be detected.");
+            }
+
+            byte[] bytes = new byte[compressedSize - 2];
+            if (stream.Read(bytes, 0, bytes.Length) < bytes.Length)
+            {
+                throw new Exception("Could not read the expected number of bytes.");
+            }
+            return DecompressZlibBytes(bytes);
+        }
+
+        /// <summary>
+        /// Decompresses zlib bytes coming after a zlib header.
+        /// </summary>
+        /// <param name="compressedBytes">Compressed bytes not including the zlib header.</param>
+        /// <returns>Decompressed data.</returns>
+        public static byte[] DecompressZlibBytes(byte[] compressedBytes)
+        {
+            using (var decompressedStream = new MemoryStream())
+            using (var compressedStream = new MemoryStream(compressedBytes))
+            using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress, true))
+            {
+                deflateStream.CopyTo(decompressedStream);
                 return decompressedStream.ToArray();
             }
         }
