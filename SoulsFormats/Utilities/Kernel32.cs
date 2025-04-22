025e56264695abeec023fa8cbe26492ccee4f6b2
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2025-04-22 12:38:09 -0400

    Sorted some more utilities, documented Kernel32.cs a bit more

--- SoulsFormats/Utilities/Kernel32.cs
+++ SoulsFormats/Utilities/Kernel32.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.ComponentModel;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace SoulsFormats
 {
-    public static class Kernel32 {
+    /// <summary>
+    /// Provides methods for loading libraries on Windows.
+    /// </summary>
+    public static class Kernel32
+    {
         [DllImport("kernel32", SetLastError = true)]
         static extern IntPtr LoadLibraryW([MarshalAs(UnmanagedType.LPWStr)]string lpFileName);
 
+        /// <summary>
+        /// Returns a string describing the last error that occurred during dynamic linking.
+        /// </summary>
         [DllImport("kernel32.dll")]
         public static extern int GetLastError();
-        public static IntPtr LoadLibrary(string path) {
-            if (!path.EndsWith(".dll")) {
+
+        /// <summary>
+        /// Loads a library with the specified filename.
+        /// </summary>
+        /// <param name="path">The name of the library to load.</param>
+        /// <returns>A handle to the loaded library, or IntPtr.Zero if the library could not be loaded.</returns>
+        public static IntPtr LoadLibrary(string path)
+        {
+            if (!path.EndsWith(".dll"))
+            {
                 path += ".dll";
             }
             return LoadLibraryW(path);
@@ -22,7 +36,8 @@ namespace SoulsFormats
     /// <summary>
     /// Provides methods for loading shared libraries on Linux.
     /// </summary>
-    public static class Libdl {
+    public static class Libdl
+    {
         [DllImport("libdl.so.2")]
         static extern IntPtr dlopen([MarshalAs(UnmanagedType.LPUTF8Str)]string filename, int flag);
 
@@ -32,9 +47,11 @@ namespace SoulsFormats
         /// <summary>
         /// Returns a string describing the last error that occurred during dynamic linking.
         /// </summary>
-        public static string GetLastError() {
+        public static string GetLastError()
+        {
             IntPtr error = dlerror();
-            if (error == IntPtr.Zero) {
+            if (error == IntPtr.Zero)
+            {
                 return null;
             }
             return Marshal.PtrToStringAnsi(error);
@@ -49,8 +66,10 @@ namespace SoulsFormats
         ///     RTLD_LAZY = 1, RTLD_NOW = 2, RTLD_GLOBAL = 256, RTLD_LOCAL = 0
         /// </param>
         /// <returns>A handle to the loaded library, or IntPtr.Zero if the library could not be loaded.</returns>
-        public static IntPtr LoadLibrary(string filename, int flag = 1) {
-            if (!filename.EndsWith(".so")) {
+        public static IntPtr LoadLibrary(string filename, int flag = 1)
+        {
+            if (!filename.EndsWith(".so"))
+            {
                 filename = "lib" + filename + ".so";
             }
             return dlopen(filename, flag);
@@ -60,20 +79,27 @@ namespace SoulsFormats
     /// <summary>
     /// Provides methods for loading native libraries and retrieving error messages.
     /// </summary>
-    public static class NativeLibrary {
+    public static class NativeLibrary
+    {
         /// <summary>
         /// Loads a native library with the specified path.
         /// </summary>
         /// <param name="path">The path to the native library to load.</param>
         /// <returns>A handle to the loaded library, or IntPtr.Zero if the library could not be loaded.</returns>
-        public static IntPtr LoadLibrary(string path) {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        public static IntPtr LoadLibrary(string path)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
                 return Kernel32.LoadLibrary(path);
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
                 Console.WriteLine("Loading Linux Library " + path);
                 return Libdl.LoadLibrary(path);
-            } else {
-                throw new PlatformNotSupportedException();
+            }
+            else
+            {
+                throw new PlatformNotSupportedException($"Unsupported Platform {RuntimeInformation.OSDescription}");
             }
         }
 
@@ -81,13 +107,19 @@ namespace SoulsFormats
         /// Retrieves the last error message from the native library loading process.
         /// </summary>
         /// <returns>A string describing the last error that occurred.</returns>
-        public static string GetLastError() {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        public static string GetLastError()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
                 var error = Kernel32.GetLastError();
                 return new Win32Exception(error).Message;
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
                 return Libdl.GetLastError();
-            } else {
+            }
+            else
+            {
                 return $"Unsupported Platform {RuntimeInformation.OSDescription}";
             }
         }
--- SoulsFormats/Utilities/Text/SFEncoding.cs
+++ SoulsFormats/Utilities/Text/SFEncoding.cs
@@ -2,14 +2,29 @@
 
 namespace SoulsFormats
 {
+    /// <summary>
+    /// Encoding for various text related things in SoulsFormats.
+    /// </summary>
     internal static class SFEncoding
     {
+        /// <summary>
+        /// ASCII encoding.
+        /// </summary>
         public static readonly Encoding ASCII;
 
+        /// <summary>
+        /// Shift-JIS encoding.
+        /// </summary>
         public static readonly Encoding ShiftJIS;
 
+        /// <summary>
+        /// UTF16 or Unicode encoding in little-endian.
+        /// </summary>
         public static readonly Encoding UTF16;
 
+        /// <summary>
+        /// UTF16 or Unicode encoding in big-endian.
+        /// </summary>
         public static readonly Encoding UTF16BE;
 
         static SFEncoding()
