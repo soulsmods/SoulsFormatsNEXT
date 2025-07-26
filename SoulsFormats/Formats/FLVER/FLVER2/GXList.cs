3dad81521a95048572040018ba3dd7972c5091c5
GompDS <79070939+GompDS@users.noreply.github.com>
2025-07-26 16:28:23 -0400

    revert gxlist stuff

--- SoulsFormats/Formats/FLVER/FLVER2/GXList.cs
+++ SoulsFormats/Formats/FLVER/FLVER2/GXList.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SoulsFormats
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public partial class FLVER2
     {
         /// <summary>
         /// A collection of items that set various material properties.
         /// </summary>
-        public class GXList : List<GXParam>
+        public class GXList : List<GXItem>
         {
             /// <summary>
             /// Value indicating the terminating item; typically int.MaxValue, sometimes -1.
@@ -30,17 +28,17 @@ namespace SoulsFormats
                 TerminatorID = int.MaxValue;
             }
 
-            internal GXList(BinaryReaderEx br, FLVER2.FLVERHeader header) : base()
+            internal GXList(BinaryReaderEx br, FLVERHeader header) : base()
             {
                 if (header.Version < 0x20010)
                 {
-                    Add(new GXParam(br, header));
+                    Add(new GXItem(br, header));
                 }
                 else
                 {
                     int id;
                     while ((id = br.GetInt32(br.Position)) != int.MaxValue && id != -1)
-                        Add(new GXParam(br, header));
+                        Add(new GXItem(br, header));
 
                     TerminatorID = br.AssertInt32(id);
                     br.AssertInt32(100);
@@ -54,7 +52,7 @@ namespace SoulsFormats
                 }
             }
 
-            internal void Write(BinaryWriterEx bw, FLVER2.FLVERHeader header)
+            internal void Write(BinaryWriterEx bw, FLVERHeader header)
             {
                 if (header.Version < 0x20010)
                 {
@@ -62,7 +60,7 @@ namespace SoulsFormats
                 }
                 else
                 {
-                    foreach (GXParam item in this)
+                    foreach (GXItem item in this)
                         item.Write(bw, header);
 
                     bw.WriteInt32(TerminatorID);
@@ -71,49 +69,12 @@ namespace SoulsFormats
                     bw.WritePattern(TerminatorLength, 0x00);
                 }
             }
-
-            public void ApplyGXListDef(GXListDef gxListDef)
-            {
-                foreach (GXParam gxParam in this)
-                {
-                    GXListDef.GXParamDef gxParamDef = gxListDef.FirstOrDefault(x => 
-                        x.ID.Equals(gxParam.ID) && x.Unk04 == gxParam.Unk04);
-                    if (gxParamDef == null) continue;
-
-                    List<GXValue> values = new List<GXValue>();
-                    
-                    BinaryReaderEx br = new BinaryReaderEx(false, gxParam.Data);
-                    foreach (GXListDef.ValueDef valueDef in gxParamDef.Items)
-                    {
-                        object val = null;
-                        switch (valueDef.Type)
-                        {
-                            case GXListDef.ValueType.Unknown:
-                            case GXListDef.ValueType.Int:
-                            case GXListDef.ValueType.Enum:
-                                val = br.ReadInt32();
-                                break;
-                            case GXListDef.ValueType.Float:
-                                val = br.ReadSingle();
-                                break;
-                            case GXListDef.ValueType.Bool:
-                                val = br.ReadInt32() != 0;
-                                break;
-                        }
-                        GXValue gxValue = new GXValue(val, valueDef);
-                        values.Add(gxValue);
-                        
-                    }
-                    
-                    gxParam.Values = values;
-                }
-            }
         }
 
         /// <summary>
         /// Rendering parameters used by materials.
         /// </summary>
-        public class GXParam
+        public class GXItem
         {
             /// <summary>
             /// In DS2, ID is just a number; in other games, it's 4 ASCII characters.
@@ -126,19 +87,14 @@ namespace SoulsFormats
             public int Unk04 { get; set; }
 
             /// <summary>
-            /// Raw parameter data, either float int values
+            /// Raw parameter data, usually just a bunch of floats.
             /// </summary>
             public byte[] Data { get; set; }
 
             /// <summary>
-            /// Named and typed values from the byte data. Non-null if an EXParamDef was applied
-            /// </summary>
-            public IReadOnlyList<GXValue> Values { get; internal set; } = null;
-
-            /// <summary>
             /// Creates a GXItem with default values.
             /// </summary>
-            public GXParam()
+            public GXItem()
             {
                 ID = "0";
                 Unk04 = 100;
@@ -148,14 +104,14 @@ namespace SoulsFormats
             /// <summary>
             /// Creates a GXItem with the given values.
             /// </summary>
-            public GXParam(string id, int unk04, byte[] data)
+            public GXItem(string id, int unk04, byte[] data)
             {
                 ID = id;
                 Unk04 = unk04;
                 Data = data;
             }
 
-            internal GXParam(BinaryReaderEx br, FLVER2.FLVERHeader header)
+            internal GXItem(BinaryReaderEx br, FLVERHeader header)
             {
                 if (header.Version <= 0x20010)
                 {
@@ -170,7 +126,7 @@ namespace SoulsFormats
                 Data = br.ReadBytes(length - 0xC);
             }
 
-            internal void Write(BinaryWriterEx bw, FLVER2.FLVERHeader header)
+            internal void Write(BinaryWriterEx bw, FLVERHeader header)
             {
                 if (header.Version <= 0x20010)
                 {
@@ -186,22 +142,6 @@ namespace SoulsFormats
                 bw.WriteInt32(Unk04);
                 bw.WriteInt32(Data.Length + 0xC);
                 bw.WriteBytes(Data);
-            }
-        }
-        
-        public class GXValue
-        {
-            public GXListDef.ValueDef ValueDef { get; }
-            public object Value { get; set; }
-            public object Min => ValueDef.Min;
-            public object Max => ValueDef.Max;
-            public string Name => ValueDef.Name;
-            public Dictionary<int, string> Enum => ValueDef.Enum;
-            
-            public GXValue(object value, GXListDef.ValueDef valueDef = null)
-            {
-                Value = value;
-                ValueDef = valueDef;
             }
         }
     }
