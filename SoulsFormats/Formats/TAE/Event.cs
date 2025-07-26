b946a56aafd9917de5763e613f7a90eb1fa63042
GompDS <79070939+GompDS@users.noreply.github.com>
2025-07-26 16:32:28 -0400

    consolidate sap br extensions into br file

--- SoulsFormats/Formats/TAE/Event.cs
+++ SoulsFormats/Formats/TAE/Event.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using SoulsAssetPipeline;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
