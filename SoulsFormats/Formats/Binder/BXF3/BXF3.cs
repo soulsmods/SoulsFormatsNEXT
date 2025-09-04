df615c62c8ccd0638e6e16b2ba894390bacc2b1d
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2025-09-03 23:01:26 -0400

    Added IsRead support for binder readers

--- SoulsFormats/Formats/Binder/BXF3/BXF3.cs
+++ SoulsFormats/Formats/Binder/BXF3/BXF3.cs
@@ -415,7 +415,7 @@ namespace SoulsFormats
         /// <summary>
         /// Whether or not the data appears to be a header file.
         /// </summary>
-        private static bool IsHeader(BinaryReaderEx br)
+        internal static bool IsHeader(BinaryReaderEx br)
             => br.Remaining >= 4 && br.GetASCII(br.Position, 4) == "BHF3";
 
         /// <summary>
